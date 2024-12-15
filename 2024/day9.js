bf20b6c99d43c058ba0f32f108fb710175feee23
Anto Joy Nellissery <antojoy23@gmail.com>
2024-12-15 13:12:28 +1030

    day 9 part 2

--- 2024/day9.js
+++ 2024/day9.js
@@ -4,21 +4,28 @@ const input = isBrowser ?
     document.body.innerText.trim() :
     `2333133121414131402`;
 
-const reversedInput = input.split('').reverse();
-
-const getFormattedBlockStringArr = () => {
+const getFormattedBlockStringArr = (part2) => {
+    const reversedInput = input.split('').reverse();
     let idx = 0;
     let formattedBlocks = [];
+    let fileInfo = [];
+    const freeSpaceCount = [];
     while (reversedInput.length > 0) {
         const fileNum = reversedInput.pop();
         formattedBlocks.push(...Array(Number(fileNum)).fill(idx));
-        ++idx;
+        part2 && fileInfo.push([idx, formattedBlocks.length - Number(fileNum), Number(fileNum)]);
         if (reversedInput.length > 0) {
             const freeSpace = reversedInput.pop();
             formattedBlocks.push(...Array(Number(freeSpace)).fill('.'));
+            part2 && freeSpaceCount.push([idx, formattedBlocks.length - Number(freeSpace), Number(freeSpace)]);
         }
+        ++idx;
     }
-    return formattedBlocks;
+    if (part2) {
+        return [formattedBlocks, fileInfo, freeSpaceCount];
+    } else {
+        return formattedBlocks;
+    }
 }
 
 const part1 = () => {
@@ -43,4 +50,32 @@ const part1 = () => {
     console.log("PART 1: ", res);
 }
 
+const part2 = () => {
+    let [formattedBlockStringArr, fileInfoList, freeSpaceCountList] = getFormattedBlockStringArr(true);
+
+    while (fileInfoList.length > 0) {
+        const [fileId, startIndex, fileCount] = fileInfoList.pop();
+        for (const [idx, [freeSpaceFileId, freeSpaceStartIndex, spaceCount]] of freeSpaceCountList.entries()) {
+            if (freeSpaceFileId === fileId) break;
+
+            if (spaceCount >= fileCount) {
+                freeSpaceCountList[idx] = [freeSpaceFileId, freeSpaceStartIndex + fileCount, spaceCount - fileCount];
+                for (let i = startIndex; i < startIndex + fileCount; i++) {
+                    formattedBlockStringArr[i] = '.';
+                }
+                for (let i = freeSpaceStartIndex; i < freeSpaceStartIndex + fileCount; i++) {
+                    formattedBlockStringArr[i] = fileId;
+                }
+                break;
+            }
+        }
+    }
+
+    const res = formattedBlockStringArr.reduce((acc, curr, idx) => curr !== '.' ? acc + (Number(curr) * idx) : acc, 0)
+
+    console.log("PART 2: ", res);
+
+}
+
 part1();
+part2();
